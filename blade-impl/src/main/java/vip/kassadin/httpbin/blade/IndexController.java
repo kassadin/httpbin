7eab04433014165b6ea2ffcbdadcf41ecd554a54
kassadin <kassadin@foxmail.com>
2018-05-01 03:48:31 +0800

    update /get

--- blade-impl/src/main/java/vip/kassadin/httpbin/blade/IndexController.java
+++ blade-impl/src/main/java/vip/kassadin/httpbin/blade/IndexController.java
@@ -1,11 +1,12 @@
 package vip.kassadin.httpbin.blade;
 
+import com.blade.mvc.WebContext;
 import com.blade.mvc.annotation.*;
 import com.blade.mvc.http.HttpMethod;
 import com.blade.mvc.http.Request;
+import com.google.common.base.Preconditions;
 
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @author kassadin
@@ -49,9 +50,8 @@ public class IndexController {
 
     @GetRoute("get")
     @JSON
-    public Map<String, Object> get(Request req, Map<String, Object> resp) {
-        resp.put("headers", req.headers());
-        return resp;
+    public Map<String, Object> get() {
+        return getMap(new String[]{"args", "headers", "origin", "url"});
     }
 
     @PostRoute("post")
@@ -82,5 +82,52 @@ public class IndexController {
         return resp;
     }
 
+    private Map<String, Object> getMap(String[] keys) {
+        String[] _keys = {"url", "args", "form", "data", "origin", "headers", "files", "json", "method"};
 
+
+        Preconditions.checkArgument(Arrays.asList(_keys).containsAll(Arrays.asList(keys)));
+
+        Request request = WebContext.request();
+
+        Map<String, Object> _map = new HashMap<>();
+        _map.put("url", getUrl(request));
+        _map.put("args", semiflatten(request.parameters()));
+        _map.put("form", request.parameters());
+        _map.put("data", request.parameters());
+        _map.put("origin", request.address());
+        _map.put("headers", request.headers());
+        _map.put("json", request.parameters());
+        _map.put("method", request.method());
+
+        Map<String, Object> outMap = new HashMap<>();
+        for (String key : keys) {
+            outMap.put(key, _map.get(key));
+        }
+
+        return outMap;
+    }
+
+    private String getUrl(Request request) {
+        String scheme = request.isSecure() ? "https" : "http";
+        String host = request.host();
+        String path = request.url();
+        return String.format("%s://%s/%s", scheme, host, path);
+    }
+
+    private Map<String, Object> semiflatten(Map<String, List<String>> params) {
+        Map<String, Object> outMpas = new HashMap<>();
+
+        List<String> value;
+        for (String key : params.keySet()) {
+            value = params.get(key);
+            if (value.size() == 1) {
+                outMpas.put(key, value.get(0));
+            } else {
+                outMpas.put(key, value);
+            }
+        }
+
+        return outMpas;
+    }
 }
